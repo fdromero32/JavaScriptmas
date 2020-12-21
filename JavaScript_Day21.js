90cf02b2cb0f84aaeb2d8d5825e97771c596b303
Danny Romero <fdromero32@gmail.com>
2020-12-21 13:02:14 -0800

    Sum of two values

---
+++ JavaScript_Day21.js
@@ -0,0 +1,16 @@
+const nums1 = [1, 2, 3];
+const nums2 = [10, 20, 30, 40];
+const value = 42;
+
+function sumOfTwo(nums1, nums2, value) {
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] + nums2[j] === value) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(sumOfTwo(nums1, nums2, value))
